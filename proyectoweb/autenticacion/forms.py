d2ca19fc7fe5e804dd1a893e4b7082a9af07326a
robinstz <3065770380401@ingenieria.usac.edu.gt>
2023-12-21 00:15:21 -0600

    v57

--- proyectoweb/autenticacion/forms.py
+++ proyectoweb/autenticacion/forms.py
@@ -11,8 +11,6 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields=['username', 'first_name', 'last_name', 'email','telefono', 'password1']
-
+        fields=['username', 'first_name', 'last_name', 'email', 'password1']
     cui = forms.IntegerField(label='CUI', help_text='Código Único de Identificación CUI')
-    telefono = forms.IntegerField(label='telefono', help_text='numero de celular')
-    profile_imagen = forms.ImageField(label='Foto de perfil', required=False)
+   
